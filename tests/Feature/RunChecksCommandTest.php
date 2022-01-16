2f17f4708b2ae3dadb4d4391257da9fdd62fb0fa
Ahmet Ozisik <ozisikahmet@gmail.com>
2022-01-16 13:33:17 +0200

    tests: add tests for eye:check command

---
+++ tests/Feature/RunChecksCommandTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+use Tests\TestCheck;
+use Appkeep\Eye\Result;
+use Appkeep\Eye\Appkeep;
+use Illuminate\Support\Facades\Http;
+
+class RunChecksCommandTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function the_check_command_is_registered()
+    {
+        // Prevent hitting Appkeep server.
+        Http::fake();
+
+        $this->artisan('eye:check')->assertExitCode(0);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_table_output()
+    {
+        Http::fake();
+
+        $appkeep = resolve(Appkeep::class);
+
+        $appkeep->checks([
+            TestCheck::make('bar')->result(
+                Result::ok()->summary('50%')
+            ),
+            TestCheck::make('baz')->result(
+                Result::warn('This is a warning')
+            ),
+        ]);
+
+        $this->artisan('eye:check')->expectsTable(
+            ['Check', 'Outcome', 'Message'],
+            [
+                ['bar', '✅ 50%', ''],
+                ['baz', '⚠️', 'This is a warning'],
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_runs_only_due_checks()
+    {
+        Http::fake();
+
+        $appkeep = resolve(Appkeep::class);
+
+        Carbon::setTestNow(Carbon::now()->setMinute(3)->setSecond(0));
+
+        $appkeep->checks([
+            TestCheck::make('test-check-1')->everyMinute(),
+            TestCheck::make('test-check-15')->everyFifteenMinutes(),
+        ]);
+
+        // Prevent hitting Appkeep server.
+        $this->artisan('eye:check')->assertExitCode(0);
+
+        // Assert that only one check ran.
+        Http::assertSent(function ($request) {
+            $data = $request->data();
+
+            return count($data['checks']) === 1
+                && $data['checks'][0]['check'] === 'test-check-1';
+        });
+
+        Carbon::setTestNow(Carbon::now()->setMinute(15)->setSecond(0));
+
+        $this->artisan('eye:check')->assertExitCode(0);
+
+        // Assert that both checks ran.
+        Http::assertSent(function ($request) {
+            $data = $request->data();
+
+            return count($data['checks']) === 2;
+        });
+    }
+}
