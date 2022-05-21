ebb38c514754327fc03ae9407f9008a440af7f80
Ahmet Ozisik <ozisikahmet@gmail.com>
2022-05-21 19:55:28 +0300

    create RunsChecks trait

--- src/Concerns/RunsChecks.php
+++ src/Concerns/RunsChecks.php
@@ -2,20 +2,31 @@
 
 namespace Appkeep\Eye\Concerns;
 
+use Appkeep\Eye\Check;
 use Appkeep\Eye\Appkeep;
+use Appkeep\Eye\Checks\DatabaseCheck;
+use Appkeep\Eye\Checks\DiskUsageCheck;
 
 trait RunsChecks
 {
-    protected function checks($callable)
-    {
-        if (! $this->app->runningInConsole()) {
-            return;
-        }
+    protected $checks = [];
 
+    protected function registerCheck(Check $check)
+    {
+        $appkeep = $this->app->make(Appkeep::class);
+        $appkeep->checks([$check]);
+    }
+
+    protected function registerDefaultChecks()
+    {
         $appkeep = $this->app->make(Appkeep::class);
 
-        $appkeep->checks(
-            call_user_func($callable)
-        );
+        $appkeep->checks([
+            DatabaseCheck::make(),
+
+            DiskUsageCheck::make()
+                ->warnIfUsedPercentageIsAbove(60)
+                ->failIfUsedPercentageIsAbove(80),
+        ]);
     }
 }
