3bfb89d4929364ee3c1c6559d1bddf7a22de7be2
Ahmet Ozisik <ozisikahmet@gmail.com>
2022-07-09 15:26:13 +0300

    add force option to appkeep:init

--- src/Commands/InitCommand.php
+++ src/Commands/InitCommand.php
@@ -7,11 +7,15 @@ use Illuminate\Support\Facades\Artisan;
 
 class InitCommand extends Command
 {
-    protected $name = 'appkeep:init';
+    protected $signature = 'appkeep:init {--force}';
     protected $description = 'Publishes config file and service provider.';
 
     public function handle()
     {
+        if ($this->option('force')) {
+            unlink(config_path('appkeep.php'));
+        }
+
         if (file_exists(config_path('appkeep.php'))) {
             $this->info('Config file is already published.');
 
