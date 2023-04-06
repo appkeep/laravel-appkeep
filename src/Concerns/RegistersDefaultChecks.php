a2c0819bd89fca04941a76027142bee2464ed075
Ahmet Ozisik <ozisikahmet@gmail.com>
2023-04-06 12:09:18 +0300

    register horizon check by default

--- src/Concerns/RegistersDefaultChecks.php
+++ src/Concerns/RegistersDefaultChecks.php
@@ -4,19 +4,20 @@ namespace Appkeep\Laravel\Concerns;
 
 use Appkeep\Laravel\Facades\Appkeep;
 use Appkeep\Laravel\Checks\CacheCheck;
+use Appkeep\Laravel\Checks\HorizonCheck;
 use Appkeep\Laravel\Checks\StorageCheck;
 use Appkeep\Laravel\Checks\DatabaseCheck;
 use Appkeep\Laravel\Checks\DiskUsageCheck;
 use Appkeep\Laravel\Checks\SystemLoadCheck;
 use Appkeep\Laravel\Checks\OptimizationCheck;
 use Appkeep\Laravel\Checks\ProductionModeCheck;
+use Laravel\Horizon\Contracts\MasterSupervisorRepository;
 
 trait RegistersDefaultChecks
 {
     protected function registerDefaultChecks()
     {
         Appkeep::checks([
-
             ProductionModeCheck::make(),
 
             StorageCheck::make(),
@@ -31,5 +32,13 @@ trait RegistersDefaultChecks
 
             SystemLoadCheck::make(),
         ]);
+
+        // Register Horizon check if it's installed
+        try {
+            app(MasterSupervisorRepository::class);
+            Appkeep::check(HorizonCheck::make());
+        } catch (\Exception $e) {
+            // Horizon not installed
+        }
     }
 }
