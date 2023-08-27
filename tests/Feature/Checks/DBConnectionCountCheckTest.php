4f59294068196dfadccbbdeb2df6c200fbf3fad9
ozank <ozankoksal@hotmail.com>
2023-08-27 10:31:54 +0300

    Added mock DB objects and tests

--- tests/Feature/Checks/DBConnectionCountCheckTest.php
+++ tests/Feature/Checks/DBConnectionCountCheckTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Checks;
 
+use Illuminate\Support\Facades\DB;
+use Mockery;
 use Tests\TestCase;
 use Appkeep\Laravel\Enums\Status;
 use Appkeep\Laravel\Checks\DBConnectionCountCheck;
@@ -13,18 +15,93 @@ class DBConnectionCountCheckTest extends TestCase
      */
     public function fails_if_connection_count_is_exceeded()
     {
-        $result = DBConnectionCountCheck::make()->setConnectionName('mysql')->run();
+        // Set DB Mock
+        DB::shouldReceive("connection")
+            ->once()
+            ->with('mysql')
+            ->andReturn(
+                Mockery::mock('Illuminate\Database\MysqlConnection', function ($mock) {
+                    $mock->shouldReceive("selectOne")
+                        ->once()
+                        ->with('show status where variable_name = "threads_connected"')
+                        ->andReturn([
+                            'Value' => 155
+                        ]);
 
-        // print_r($result);
+                    $mock->shouldReceive("selectOne")
+                        ->once()
+                        ->with("show variables like 'max_connections'")
+                        ->andReturn([
+                            'Value' => 155
+                        ]);
+                })
+            );
 
-        // $result = DatabaseCheck::make()->run();
-        // $this->assertEquals(Status::FAIL, $result->status);
-        // $this->assertStringStartsWith('Could not connect to DB:', $result->message);
+
+        $result = DBConnectionCountCheck::make()->setConnectionName('mysql')->warnIfConnectionCountIsAbove(35)->run();
+        $this->assertEquals(Status::FAIL, $result->status);
     }
     /**
      * @test
      */
     public function warns_if_connection_count_is_exceeded()
     {
+        // Set DB Mock
+        DB::shouldReceive("connection")
+            ->once()
+            ->with('mysql')
+            ->andReturn(
+                Mockery::mock('Illuminate\Database\MysqlConnection', function ($mock) {
+                    $mock->shouldReceive("selectOne")
+                        ->once()
+                        ->with('show status where variable_name = "threads_connected"')
+                        ->andReturn([
+                            'Value' => 50
+                        ]);
+
+                    $mock->shouldReceive("selectOne")
+                        ->once()
+                        ->with("show variables like 'max_connections'")
+                        ->andReturn([
+                            'Value' => 155
+                        ]);
+                })
+            );
+
+
+        $result = DBConnectionCountCheck::make()->setConnectionName('mysql')->warnIfConnectionCountIsAbove(35)->run();
+        $this->assertEquals(Status::WARN, $result->status);
+    }
+
+    /**
+     * @test
+     */
+    public function succeed_if_connection_count_is_below_warn_limit()
+    {
+        // Set DB Mock
+        DB::shouldReceive("connection")
+            ->once()
+            ->with('mysql')
+            ->andReturn(
+                Mockery::mock('Illuminate\Database\MysqlConnection', function ($mock) {
+                    $mock->shouldReceive("selectOne")
+                        ->once()
+                        ->with('show status where variable_name = "threads_connected"')
+                        ->andReturn([
+                            'Value' => 8
+                        ]);
+
+                    $mock->shouldReceive("selectOne")
+                        ->once()
+                        ->with("show variables like 'max_connections'")
+                        ->andReturn([
+                            'Value' => 155
+                        ]);
+                })
+            );
+
+
+        $result = DBConnectionCountCheck::make()->setConnectionName('mysql')->warnIfConnectionCountIsAbove(35)->run();
+        $this->assertEquals(Status::OK, $result->status);
     }
 }
