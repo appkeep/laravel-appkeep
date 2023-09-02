f463ff44877c6bd6de6d96cab7afefaa5132f228
ozank <ozankoksal@hotmail.com>
2023-09-02 23:21:35 +0300

    Added method to collect events from file

--- src/Listeners/SlowQueryHandler.php
+++ src/Listeners/SlowQueryHandler.php
@@ -2,18 +2,24 @@
 
 namespace Appkeep\Laravel\Listeners;
 
+use Exception;
+
 class SlowQueryHandler
 {
     static $fileName = "tmp_slow_query.log";
-    public static function handle($filename,  $event)
+    public static function handle($filename,  $event, $context)
     {
         $fp = fopen($filename, "a+");
 
-        $eventInfo = json_encode(array_filter((array)$event, function ($key) {
+        $slowQueryEvent = [
+            'event' => $event,
+            'context' => $context,
+        ];
+
+        $eventInfo = json_encode(array_filter($slowQueryEvent, function ($key) {
             return $key != 'connection';
         }, ARRAY_FILTER_USE_KEY));
 
-        // dd($toAppend);
         if (flock($fp, LOCK_EX)) {
             // Truncate last bracket of json
             $stat = fstat($fp);
@@ -31,5 +37,32 @@ class SlowQueryHandler
         }
 
         fclose($fp);
+    }
+
+    public static function collectSlowQueriesFromFile(string $filename, bool $clear)
+    {
+        if (file_exists($filename)) {
+
+            $data = [];
+            $fp = fopen($filename, "r+");
+
+            if (flock($fp, LOCK_EX)) {
+                $stat = fstat($fp);
+                if (!$stat['size'] == 0) {
+                    $contents = fread($fp, $stat['size']);
+                    $data = json_decode($contents, true);
+                    if ($clear) {
+                        ftruncate($fp, 0);
+                    }
+                    fflush($fp);
+                }
+                flock($fp, LOCK_UN);
+            }
+
+            fclose($fp);
+            return $data;
+        } else {
+            throw new Exception('File does not exist');
+        }
     }
 }
